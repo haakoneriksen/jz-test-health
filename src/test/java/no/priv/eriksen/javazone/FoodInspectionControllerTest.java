0636a0ec689220830a1ce46da4cdf79fb85aa9f9
haaeriks <haakon.gjersvik.eriksen@finn.no>
2016-08-16 10:52:42 +0200

    upgrade spring boot to 1.4, implement integration test

--- src/test/java/no/priv/eriksen/javazone/FoodInspectionControllerTest.java
+++ src/test/java/no/priv/eriksen/javazone/FoodInspectionControllerTest.java
@@ -1,12 +1,42 @@
 package no.priv.eriksen.javazone;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
 public class FoodInspectionControllerTest {
 
-    @Test
-    public void canGetInspectionByOrgId() throws Exception {
+    private MockMvc mvc;
 
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new FoodInspectionController()).build();
+    }
+
+    @Test
+    public void getTilsynByPostCode() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/tilsyn/postnr/9950").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[{" +
+                        "\"name\":\"Abc Thai AS\"," +
+                        "\"orgId\":985129576," +
+                        "\"address\":\"Christian IV gate 3\"," +
+                        "\"postCode\":9950," +
+                        "\"postName\":\"VARDØ\"," +
+                        "\"inspectionResult\":1" +
+                        "}]")));
     }
 
 }
