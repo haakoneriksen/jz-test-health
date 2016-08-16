f971fdda7be1b27c134bb8d62c94ab07feb60d7a
haaeriks <haakon.gjersvik.eriksen@finn.no>
2016-08-16 16:03:53 +0200

    check json values in integration test instead of json document as string

--- src/test/java/no/priv/eriksen/javazone/FoodInspectionControllerTest.java
+++ src/test/java/no/priv/eriksen/javazone/FoodInspectionControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -29,14 +29,12 @@ public class FoodInspectionControllerTest {
     public void getTilsynByPostCode() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/tilsyn/postnr/9950").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("[{" +
-                        "\"name\":\"Abc Thai AS\"," +
-                        "\"orgId\":985129576," +
-                        "\"address\":\"Christian IV gate 3\"," +
-                        "\"postCode\":9950," +
-                        "\"postName\":\"VARDØ\"," +
-                        "\"inspectionResult\":1" +
-                        "}]")));
+                .andExpect(jsonPath("$.[0].name").value(equalTo("Abc Thai AS")))
+                .andExpect(jsonPath("$.[0].orgId").value(equalTo(985129576)))
+                .andExpect(jsonPath("$.[0].address").value(equalTo("Christian IV gate 3")))
+                .andExpect(jsonPath("$.[0].postCode").value(equalTo(9950)))
+                .andExpect(jsonPath("$.[0].postName").value(equalTo("VARDØ")))
+                .andExpect(jsonPath("$.[0].inspectionResult").value(equalTo(1)));
     }
 
 }
